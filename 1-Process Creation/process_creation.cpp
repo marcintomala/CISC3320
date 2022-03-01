dec942753e8ce6c3b3bef51e1e18ac60efbe449b
Marcin Tomala <marcin.p.tomala@gmail.com>
2022-02-28 23:22:29 -0500

    fixed the console logouts to fit the assignment specification

--- 1-Process Creation/process_creation.cpp
+++ 1-Process Creation/process_creation.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 
 int main () {
-    pid_t pid = fork();
-    if (pid == 0) {
-        pid = fork();
-        if (pid == 0) {
-           cout << "Hello World, I'm the grandchild process and my Process id is: " << getpid() << "\n";
+    pid_t id = fork(); // A child process is created and the id is assigned to the variable id
+    if (id == 0) { // if the id is 0, the process is the forked C (child) process; otherwise, it is the P (parent) process
+        id = fork(); // if we're in the child process, we fork again to create a grandchild
+        if (id == 0) { // same logic applies; if fork() returns 0, we're in the GC (grandchild) process, otherwise - in the middle process
+           cout << "I'm the grandchild process and my process id is: " << getpid() << endl;
         } else {
-            cout << "Hello World, I'm the child process and my Process id is: " << getpid() << "\n";
+            cout << "I'm the parent process and my process id is: " << getpid() << endl;
         }
     } else {
-        cout << "Hello World, I'm the parent process and my Process id is: " << getpid() << "\n";
+        cout << "I'm the grandparent process and my process id is: " << getpid() << endl;
     }
 
     return 0;
@@ -23,8 +23,8 @@ int main () {
 /*
 Sample Output:
 
-Hello World, I'm the parent process and my Process id is: 1847
-Hello World, I'm the child process and my Process id is: 1848
-Hello World, I'm the grandchild process and my Process id is: 1849
+I'm the grandparent process and my process id is: 499
+I'm the parent process and my process id is: 500
+I'm the grandchild process and my process id is: 501
 
 */
